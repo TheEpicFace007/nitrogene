fc8be5d660c561ad35208dc48c5611738f936540
Vini Dalvino <vinidalvino@cock.li>
2020-04-01 12:38:58 -0400

    added the parameter for the search result post request

--- searchApi.lua
+++ searchApi.lua
@@ -1,3 +1,28 @@
+-- https://developers.google.com/custom-search/v/introduction#identify_your_application_to_google_with_api_key
+-- https://any-api.com/googleapis_com/customsearch/docs/cse/search_cse_siterestrict_list
 local HttpService = game:GetService("HttpService")
 api = {}
-local endpoint = "https://www.googleapis.com/customsearch/v1"
+
+api.parseSearch = function (searchTerm)
+    searchTerm = string.gsub(searchTerm," ","+")
+    local searchResult = game:HttpPostAsync(
+        "https://duckduckgo.com/lite/" ,                      -- url
+        "q=site%3Apastebin.com+" .. searchTerm .. "&kl=&df=", -- post data
+        "text/html; charset=UTF-8",                           -- content type
+        true,                                                   -- comptess
+        [[
+Host: duckduckgo.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br
+Referer: https://duckduckgo.com/
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 44
+Origin: https://duckduckgo.com
+DNT: 1
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1
+        ]]
+    )
+end
